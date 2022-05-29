3e63c5b336cd5b989ec43ce94c702b3534cb3d08
RishiDarkDevil <rishi8001100192@gmail.com>
2022-05-29 10:41:16 +0530

    Poly Regression of Descent on Ascent

--- SLR-Comp-ROIs.R
+++ SLR-Comp-ROIs.R
@@ -15,7 +15,7 @@ BOLD_data_des_avg <- BOLD_data_des %>%
 BOLD_data_avg <- bind_rows(BOLD_data_asc_avg, BOLD_data_des_avg)
 
 # Models all the Descents on Ascents by specified ROI
-model_des_on_asc <- function(data, ROI_name) {
+model_des_on_asc <- function(data, ROI_name, smt = 1) {
   data <- data %>%
     filter(ROI == ROI_name)
   
@@ -27,14 +27,14 @@ model_des_on_asc <- function(data, ROI_name) {
     data_model <- data_model %>%
       spread(phase, BOLD_mean)
     
-    models[[i]] <- lm(as.formula(str_c("D",i,"~","A",i)), data = data_model)
+    models[[i]] <- lm(as.formula(str_c("D",i,"~poly(","A",i,",",smt,")")), data = data_model)
   }
   
   return(models)
 }
 
 # Visualizae ROI Regression
-vis_des_on_asc <- function(data, ROI_name, mods = NULL) {
+vis_des_on_asc <- function(data, ROI_name, mods = NULL, smt = 1) {
   data <- data %>%
     filter(ROI == ROI_name)
   
@@ -49,7 +49,7 @@ vis_des_on_asc <- function(data, ROI_name, mods = NULL) {
     plots[[i]] <- data_model %>%
       ggplot(aes(data_model[[str_c("A",i)]], data_model[[str_c("D",i)]])) +
       geom_point(size = 1.5) +
-      geom_smooth(method = "lm", formula = y~x, se = FALSE, size = 2.5) +
+      geom_smooth(method = "lm", formula = y~poly(x,smt), se = FALSE, size = 2.5) +
       labs(
         x = str_c("A",i),
         y = str_c("D",i)
@@ -83,20 +83,22 @@ ROIs <- str_c(c("VC", "l_OFC", "m_OFC"), "_1")
 model_ROIs <- ROIs %>%
   map(~model_des_on_asc(model_data, .))
 
-# Visualize the BOLD Descent on Ascent for the different ROIs
-plot_ROIs <- list()
+smt <- c(6,6,5)
+model_ROIs <- list()
 for (i in seq_along(ROIs)) {
-  plot_ROIs[[i]] <- vis_des_on_asc(model_data, ROIs[i], model_ROIs[[i]])
+  model_ROIs[[i]] <- model_des_on_asc(model_data, ROIs[i], smt[i])
 }
 
+# Visualize the BOLD Descent on Ascent for the different ROIs
+
 # VC Visualization
-plot_VC <- ggarrange(plotlist = plot_ROIs[[1]], ncol = 3, nrow = 2)
+plot_VC <- ggarrange(plotlist = vis_des_on_asc(model_data, ROIs[1], model_ROIs[[1]], smt[1]), ncol = 3, nrow = 2)
 annotate_figure(plot_VC, top = text_grob("VC BOLD Descent on Ascent", face = "bold", size = 16))
 
 # l_OFC Visualization
-plot_l_OFC <- ggarrange(plotlist = plot_ROIs[[2]], ncol = 3, nrow = 2)
+plot_l_OFC <- ggarrange(plotlist = vis_des_on_asc(model_data, ROIs[2], model_ROIs[[2]], smt[2]), ncol = 3, nrow = 2)
 annotate_figure(plot_l_OFC, top = text_grob("l_OFC BOLD Descent on Ascent", face = "bold", size = 16))
 
 # m_OFC Visualization
-plot_m_OFC <- ggarrange(plotlist = plot_ROIs[[3]], ncol = 3, nrow = 2)
+plot_m_OFC <- ggarrange(plotlist = vis_des_on_asc(model_data, ROIs[3], model_ROIs[[3]], smt[3]), ncol = 3, nrow = 2)
 annotate_figure(plot_m_OFC, top = text_grob("m_OFC BOLD Descent on Ascent", face = "bold", size = 16))
